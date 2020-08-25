d61e4316810d02732bd3cc2f1c7164bf56283bca
kalaiselvib <52741338+kalaiselvib@users.noreply.github.com>
2020-08-25 21:03:49 +0530

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,61 @@
 
 
 
-$('.carousel').carousel({
-    interval: 4000
-  })
 
-  $(document).ready(function () {
-    $(".gallery-img").click(function(){
-      var t = $(this).attr("src");
-      $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-      $("#myModal").modal();
-    });
-  });
+
+
+
+
+
+
+
+
+
+
+$('.carousel').carousel({
+  interval: 4000
+})
+
+
+  var counter = 1;
+  setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+      counter = 1;
+    }
+  }, 3000);
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-opacity-off";
+}
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
