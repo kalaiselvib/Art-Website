cfbf4badbcb6190b76fbc13931e8a032bf23a4b1
kalaiselvi <kalaiselvi993@gmail.com>
2020-10-03 10:04:21 +0530

    previous

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,3 +60,21 @@ function carousel() {
   x[slideIndex-1].style.display = "block";
   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
+
+$(document).ready(function() {
+  var $lightbox = $('#lightbox');
+  $('[data-target="#lightbox"]').on('click', function(event) {
+      var $img = $(this).find('img'), 
+          src = $img.attr('src'),
+          alt = $img.attr('alt');
+         
+      $lightbox.find('img').attr('src', src);
+      $lightbox.find('img').attr('alt', alt);
+      $lightbox.find('img').css(css);
+  });
+  $lightbox.on('shown.bs.modal', function (e) {
+      var $img = $lightbox.find('img');
+      $lightbox.find('.modal-dialog').addClass('image');
+      $lightbox.find('.close').removeClass('hidden');
+  });
+});
